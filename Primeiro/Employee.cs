2defc70aff4cb28f2bacd35dd35ed2b208a862fc
Walyson do Prado Fernandes <walyson.prado@hotmail.com>
2020-11-11 17:30:18 -0300

    Projeto iniciado, calculando o valor do desconto

---
+++ Primeiro/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Primeiro
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Employee(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void EmployeeData() => Console.WriteLine(Id + " - " + Nome + " - " + Salario.ToString("F2", CultureInfo.InvariantCulture));
+        
+        public void AumentoSalarial(double valor)
+        {
+            double Valor = (valor / 100) + 1;
+            Salario *= Valor;
+        }
+
+
+    }
+}
---
+++ Primeiro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double preco = double.Parse(Console.ReadLine());
+
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+            Console.WriteLine(desconto);
+
+        }
+
+    }
+}
